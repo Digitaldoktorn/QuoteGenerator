ab65a480e01cca1be16bd3eed5d3b1c8a7219de2
Anders Eriksson <digitaldoktorn@gmail.com>
2021-02-11 18:12:07 +0100

    adjust fontsize based on data length - not working

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 function renderWelcomeScreen() {
   document.getElementById('welcome').innerHTML = `
         <h3>Welcome!</h3>
-        <p>Enrich yourself with random quotes from the quotable API. <br>Pause if you need to with the "STOP" button.</p>    
+        <p>Enrich yourself with random quotes from the quotable API. <br>Pause and start if you need to with the buttons.</p>    
     `
 }
 
 async function generateQuotes() {
   const response = await fetch('https://api.quotable.io/random')
   const data = await response.json()
-  document.getElementById('content').innerHTML = data.content
+  if (data.length <= 100) {
+    document.getElementById('content').innerHTML = data.content
+  }
+  if (data.length > 100) {
+    document.getElementById('content-long').innerHTML = data.content
+  }
+
   document.getElementById('author').innerHTML = data.author
   document.getElementById('tag').innerHTML = data.tags.join(' | ')
   console.log(data.length)
