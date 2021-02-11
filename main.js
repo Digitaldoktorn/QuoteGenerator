b68aae88f5fda04ac6c77c1e09ac9f6355ec04c7
Anders Eriksson <digitaldoktorn@gmail.com>
2021-02-11 17:52:59 +0100

    start and stop etc

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+function renderWelcomeScreen() {
+  document.getElementById('welcome').innerHTML = `
+        <h3>Welcome!</h3>
+        <p>Enrich yourself with random quotes from the quotable API. <br>Pause if you need to with the "STOP" button.</p>    
+    `
+}
+
 async function generateQuotes() {
   const response = await fetch('https://api.quotable.io/random')
   const data = await response.json()
   document.getElementById('content').innerHTML = data.content
   document.getElementById('author').innerHTML = data.author
   document.getElementById('tag').innerHTML = data.tags.join(' | ')
-  //   console.log(data.author)
-  //   console.log(data.content)
   console.log(data.length)
   //   data.tags.map(function (x) {
   //     console.log('Tag: ' + x)
@@ -14,5 +19,5 @@ async function generateQuotes() {
   //     console.log(element)
   //   })
 }
-generateQuotes()
+renderWelcomeScreen()
 let start = setInterval(generateQuotes, 9000)
