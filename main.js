efc411903a0d85949ce72ce89ce45a24321b2747
Anders Eriksson <digitaldoktorn@gmail.com>
2021-03-03 14:08:25 +0100

    design improvements

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
-function renderWelcomeScreen() {
-  document.getElementById('welcome').innerHTML = `
-        <h3>Welcome!</h3>
-        <p>Enrich yourself with random quotes from the quotable API. <br>Pause and start if you need to with the buttons.</p>    
-    `
-}
+// function renderWelcomeScreen() {
+//   document.getElementById('welcome').innerHTML = `
+//         <h3>Welcome!</h3>
+//         <p>Start and pause with the buttons.</p>
+//     `
+// }
 
 async function generateQuotes() {
   const response = await fetch('https://api.quotable.io/random')
   const data = await response.json()
-  if (data.length <= 100) {
-    document.getElementById('content').innerHTML = data.content
-  }
-  if (data.length > 100) {
-    document.getElementById('content-long').innerHTML = data.content
-  }
-
+  document.getElementById('content').innerHTML = data.content
   document.getElementById('author').innerHTML = data.author
   document.getElementById('tag').innerHTML = data.tags.join(' | ')
-  console.log(data.length)
+  // console.log(data.length)
   //   data.tags.map(function (x) {
   //     console.log('Tag: ' + x)
   //   })
@@ -25,5 +19,5 @@ async function generateQuotes() {
   //     console.log(element)
   //   })
 }
-renderWelcomeScreen()
-let start = setInterval(generateQuotes, 9000)
+// renderWelcomeScreen()
+let start = setInterval(generateQuotes, 12000)
