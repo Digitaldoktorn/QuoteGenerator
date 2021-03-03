89c424b698ee75dadfa79917b48180697db740ca
Anders Eriksson <digitaldoktorn@gmail.com>
2021-03-03 14:18:27 +0100

    filtering out long quotes

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-// function renderWelcomeScreen() {
-//   document.getElementById('welcome').innerHTML = `
-//         <h3>Welcome!</h3>
-//         <p>Start and pause with the buttons.</p>
-//     `
-// }
-
 async function generateQuotes() {
   const response = await fetch('https://api.quotable.io/random')
   const data = await response.json()
-  document.getElementById('content').innerHTML = data.content
-  document.getElementById('author').innerHTML = data.author
-  document.getElementById('tag').innerHTML = data.tags.join(' | ')
-  // console.log(data.length)
-  //   data.tags.map(function (x) {
-  //     console.log('Tag: ' + x)
-  //   })
-  //   data.tags.forEach((element) => {
-  //     console.log(element)
-  //   })
+
+  if (data.length < 150) {
+    document.getElementById('content').innerHTML = data.content
+    document.getElementById('author').innerHTML = data.author
+    document.getElementById('tag').innerHTML = data.tags.join(' | ')
+    console.log(data.length)
+  } else {
+    return null
+  }
 }
-// renderWelcomeScreen()
+
 let start = setInterval(generateQuotes, 12000)
